1872160ecc08243a13f27aba8e0ba8d6d6297070
pwn0x80 <aditya0x80@gmail.com>
2023-06-24 01:51:12 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+// import './App.css';
+import { Navbar } from "@components/Home/Navbar";
+
+function App() {
+  return (
+      <div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
