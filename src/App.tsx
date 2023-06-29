4d98c6230b0f78db8f36e3011981d4f158b5c23b
pwn0x80 <aditya0x80@gmail.com>
2023-06-29 08:03:15 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 // import './App.css';
 import { Navbar } from "@components/Home/Navbar";
+import { MobBuyerNavbar } from '@components/NavBar/MobBuyerNavbar';
+import { applyTheme, createTheme } from '@components/theme/utils';
+import darkTheme from '@components/theme/darktheme';
+import baseTheme from '@components/theme/base';
+import { BuyerNavbar } from '@components/NavBar/BuyerNavbar';
 
 function App() {
+  useEffect(()=>{
+    applyTheme(baseTheme)
+    // applyTheme(darkTheme)
+  },[])
   return (
       <div>
-      <Navbar/>
+      <BuyerNavbar/>
+      <MobBuyerNavbar />
     </div>
   );
 }
