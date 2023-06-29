3cadc1656c9e0ddd240eab35c637fda0fbbc5096
pwn0x80 <aditya0x80@gmail.com>
2023-06-29 07:09:48 +0530

    svg-icon

---
+++ src/core-ui/Icons/IconsSvg.tsx
@@ -0,0 +1,26 @@
+const SearchLenSvg = ({ color }: any) => {
+  return (
+    <svg aria-hidden="true" className="self-center w-5 h-5 text-gray-500 dark:text-gray-400" fill={color} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+  )
+}
+const CartSvg = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+    </svg>
+  )
+}
+const HomeSvg = () => {
+  return (
+    <svg className="w-6 h-6 mb-1 text-white fill-btnBottomNavbar  group-hover:text-blue-600 0" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+      <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+    </svg>
+  )
+}
+
+const ProfileSvg = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+  </svg>
+)
+export { SearchLenSvg, CartSvg, HomeSvg,ProfileSvg }
---
+++ src/core-ui/index.tsx
@@ -0,0 +1,2 @@
+import { SearchLenSvg,CartSvg,HomeSvg,ProfileSvg } from "./Icons/IconsSvg"
+export {SearchLenSvg,CartSvg,HomeSvg,ProfileSvg};
